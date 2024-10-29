cc7fe246703ffa7ad317addf4fbe6d16fa55cafc
SushmaN04 <144507078+SushmaN04@users.noreply.github.com>
2024-10-29 15:28:07 -0500

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -2,17 +2,17 @@ create or replace database mahe_db;
 create or REPLACE schema mahe_schema;
 
 
--- create_tables.sql
-CREATE OR REPLACE TABLE source_customers (
+CREATE OR REPLACE TABLE mahe_db.mahe_schema.source_customers (
     customer_id INT,
     customer_name STRING,
     email STRING,
     city STRING
 );
 
-CREATE OR REPLACE TABLE target_customers (
+CREATE OR REPLACE TABLE mahe_db.mahe_schema.target_customers (
     customer_id INT,
     customer_name STRING,
     email STRING,
     city STRING
 );
+
